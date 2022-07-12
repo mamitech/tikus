740d0c86956db3f6b9971e79ecd06c4fe42a888c
Gilbok Lee <gilbok@live.com>
2022-07-13 00:55:14 +0700

    add tikus:ping command

---
+++ src/Console/PingCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mamikos\Tikus\Console;
+
+use Illuminate\Console\Command;
+use Mamikos\Tikus\Tikus;
+use Exception;
+
+class PingCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tikus:ping {message=ping} {--as-exception}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Ping to the remote systems registered in Tikus';
+
+    public function handle()
+    {
+        $message = $this->argument('message');
+        if (empty($message) === true)
+        {
+            $this->error('message cannot be blank!');
+            return 1;
+        }
+
+        $asException = $this->option('as-exception');
+
+        if ($asException === true)
+        {
+            Tikus::reportException(new Exception($message));
+            $this->info('An exception has been sent!');
+        }
+        else
+        {
+            Tikus::reportError('Ping', $message);
+            $this->info('A message has been sent!');
+        }
+    }
+}
